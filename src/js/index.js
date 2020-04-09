6107da603088601b3859c28f080858308e7c9aa4
ManeoLin <nixsherrington@gmail.com>
2020-04-08 23:40:43 -0400

    Update src/js/index.js to accept multiple properties

--- src/js/index.js
+++ src/js/index.js
@@ -21,36 +21,38 @@ dp.datepicker().on('changeDate', function () {
         dp.datepicker('getFormattedDate')
     );
     currentDate = prepareDate(dp.datepicker('getDate'));
-    d3.json('https://covidtracking.com/api/states/daily', jsonData => updateChart(jsonData, currentDate, property));
+    d3.json('https://covidtracking.com/api/states/daily', jsonData => updateChart(jsonData, currentDate, properties));
 });
 
 sp.selectpicker();
 sp.on('changed.bs.select', function () {
-    let selectedValue = $(this).val();
-    switch (selectedValue) {
-        case 'increase':
-            property = 'totalTestResultsIncrease';
-            break;
-        case 'positive':
-            property = 'positive';
-            break;
-        case 'negative':
-            property = 'negative';
-            break;
-        case 'pending':
-            property = 'pending';
-            break;
-        case 'hospitalized':
-            property = 'hospitalized';
-            break;
-        case 'death':
-            property = 'death';
-            break;
+    let selectedValues = $(this).val();
+    for (const selectedValue of selectedValues) {
+        switch (selectedValue) {
+            case 'increase':
+                properties.push('totalTestResultsIncrease');
+                break;
+            case 'positive':
+                properties.push('positive');
+                break;
+            case 'negative':
+                properties.push('negative');
+                break;
+            case 'pending':
+                properties.push('pending');
+                break;
+            case 'hospitalized':
+                properties.push('hospitalized');
+                break;
+            case 'death':
+                properties.push('death');
+                break;
+        }
     }
-    d3.json('https://covidtracking.com/api/states/daily', jsonData => updateChart(jsonData, currentDate, property));
+    d3.json('https://covidtracking.com/api/states/daily', jsonData => updateChart(jsonData, currentDate, properties));
 });
 
 // Initialize
 let currentDate = prepareDate(dp.datepicker('getDate'));
-let property = 'totalTestResultsIncrease';
-newChart(currentDate, property);
+let properties = ['totalTestResultsIncrease'];
+newChart(currentDate, properties);
